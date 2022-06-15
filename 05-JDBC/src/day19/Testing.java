f15a13ec11c53539088cd24453dc902a07363148
Dell <shuns5199@gmail.com>
2022-06-15 09:12:27 +0630

    Manage Transaction

---
+++ 05-JDBC/src/day19/Testing.java
@@ -0,0 +1,17 @@
+package day19;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+
+public class Testing {
+
+	public static void main(String[] args) {
+		try (Connection con = 
+				DriverManager.getConnection("jdbc:mariadb://localhost:3306/level3_batch2", "root", "")) {
+			System.out.println("Get Connection");
+			
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+	}
+}
